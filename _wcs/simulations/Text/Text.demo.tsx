e7b15e3efe7cdad11b9a0202ec02651a4da03241
Ari Kishon <ariki@wix.com>
2022-01-31 14:14:23 +0200

    chore: add Demo for text

---
+++ _wcs/simulations/Text/Text.demo.tsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import { createDemo } from '@wixc3/react-simulation';
+import { Text } from '../../../src/components/text/text';
+
+export default createDemo({
+    name: 'Text',
+    demo: () => <Text>I Am a text</Text>
+});
