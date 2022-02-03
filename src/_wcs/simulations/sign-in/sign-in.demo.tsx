e54637e9e8a6048d6fa37f6f266af4e042b91faa
Cario <carioh@wix.com>
2022-02-03 19:28:02 +0200

    new demo component for alpha

---
+++ src/_wcs/simulations/sign-in/sign-in.demo.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { createDemo } from '@wixc3/react-simulation';
+import { SignIn } from './../../../components/sign-in/sign-in';
+
+export default createDemo({
+    name: 'SignIn',
+    demo: () => <SignIn passwordInputPlaceholder={'gfgf'}></SignIn>,
+    environmentProps: {
+        windowHeight: 573,
+        windowBackgroundColor: '#ffffff',
+        windowWidth: 1024
+    }
+});
