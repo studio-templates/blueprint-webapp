35f11f67c7f646bb95e8fccc42001088dbcdbf6c
Peter Shershov <peters@wix.com>
2022-02-07 12:14:36 +0200

    renaming files and adding ''_wcs" to tsconfig

--- _wcs/simulations/ButtonGroup/ButtonGroup.demo.tsx
+++ _wcs/simulations/ButtonGroup/ButtonGroup.demo.tsx
@@ -1,10 +1,16 @@
-
-import React from 'react';
-import { createDemo } from '@wixc3/react-simulation';
-import { ButtonGroup } from '../../../src/components/button-group/button-group';
-import { Button } from '../../../src/components/Button/Button';
+import React from "react";
+import { createDemo } from "@wixc3/react-simulation";
+import { ButtonGroup } from "../../../src/components/button-group/button-group";
+import { Button } from "../../../src/components/button/button";
 
 export default createDemo({
-    name: 'ButtonGroup',
-    demo: () => <ButtonGroup><Button icon="emoji"/><Button icon="emoji"/><Button icon="emoji"/><Button icon="emoji"/></ButtonGroup>
+  name: "ButtonGroup",
+  demo: () => (
+    <ButtonGroup>
+      <Button icon="emoji" />
+      <Button icon="emoji" />
+      <Button icon="emoji" />
+      <Button icon="emoji" />
+    </ButtonGroup>
+  ),
 });
