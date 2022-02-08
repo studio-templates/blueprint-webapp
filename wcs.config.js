155c53c60336a8fba86b6d5eb1b7c9a38603ee3d
Ari Kishon <37385467+Ari-Kishon@users.noreply.github.com>
2022-02-08 20:01:50 +0200

    chore: update wcs.config

--- wcs.config.js
+++ wcs.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-    simulationGlobalSetup: './_wcs/simulation-global-setup.ts',
+    simulationGlobalSetup: "./_wcs/simulation-global-setup.ts",
     newComponent: {
-        componentsPath: './src/components',
-        templatesPath: './src/component-templates',
+      componentsPath: "./src/components",
+      templatesPath: "./src/component-templates",
     },
-}
+    sassCompilation: {
+      modules: {
+        include: ["**/*.module.scss"],
+      },
+    },
+    cssCompilation: {
+      cssModulesExt: ['.module.scss']
+    }
+};
