3566d69f8d9b56a0a7c283b6432dc7d96c50035e
Peter Shershov <peters@wix.com>
2022-02-08 20:46:22 +0200

    indentation

--- wcs.config.js
+++ wcs.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  boardGlobalSetup: "./_wcs/board-global-setup.ts",
+    boardGlobalSetup: "./_wcs/board-global-setup.ts",
     newComponent: {
       componentsPath: "./src/components",
       templatesPath: "./src/component-templates",
