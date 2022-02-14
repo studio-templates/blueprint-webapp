c472a19862afc60776eeec1b6e56c3344547afcb
Ari Kishon <37385467+Ari-Kishon@users.noreply.github.com>
2022-02-14 17:12:26 +0200

    chore: move boards to correct folder (#10)

--- _wcs/boards/mini-cards/mini-cards.board.tsx
+++ _wcs/boards/mini-cards/mini-cards.board.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { createBoard } from '@wixc3/react-board';
-import { MiniCards } from '../../../components/mini-cards/mini-cards';
+import { MiniCards } from '../../../src/components/mini-cards/mini-cards';
 
 export default createBoard({
     name: 'Example of mini panels',
