44aa8028a8a861d9b2eefd2cbf682b67b8b400fe
idango10 <idango@wix.com>
2022-02-15 13:05:39 +0200

    added props to receiver

--- src/components/receiver/receiver.tsx
+++ src/components/receiver/receiver.tsx
@@ -3,9 +3,11 @@ import { IconContainer } from '../icon-container/icon-container';
 
 export interface ReceiverProps {
     className?: string;
+    icon: string;
+    name: string;
 }
 
-export const Receiver: React.FC<ReceiverProps> = ({ className = '' }) => (
+export const Receiver: React.FC<ReceiverProps> = ({ icon, name, className = '' }) => (
     <div className={`${className} receiver`}>
-        <IconContainer icon={'shopping-cart'} className="iconContainerReceiver" />Receiver</div>
+        <IconContainer icon={icon} className="iconContainerReceiver" />{name}</div>
 );
