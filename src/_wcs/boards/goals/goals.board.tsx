b613c6c04f95e0784c4a8dfb9d9f6c78d9cbba96
eifr <ofirl@wix.com>
2022-02-15 13:43:09 +0200

    add goal props

--- src/_wcs/boards/goals/goals.board.tsx
+++ src/_wcs/boards/goals/goals.board.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import { createBoard } from "@wixc3/react-board";
 import { Goal } from "../../../components/goal/goal";
 
+const goal: Goal = {
+        category: 'Holiday',
+        date: '15/12/2009',
+        icon: 'add',
+        price: 550
+    }
+
 export default createBoard({
-  name: "Goals",
-  Board: () => <Goal />,
-  environmentProps: {
-    canvasWidth: 208,
-    canvasHeight: 215,
-  },
+    name: "Goals",
+    Board: () => <Goal goal={goal} />,
+    environmentProps: {
+        canvasWidth: 208,
+        canvasHeight: 215,
+    },
 });
--- src/components/goal/goal.tsx
+++ src/components/goal/goal.tsx
@@ -1,22 +1,34 @@
 import React from "react";
-import styles from "./goals.module.scss";
+import styles from "./goal.module.scss";
 import { ShadowCard } from "../shadow-card/shadow-card";
 import { Icon } from "../icon/Icon";
+import { IconName } from "@blueprintjs/icons";
 
-export interface GoalsProps {
+export interface Goal {
+  price: number;
+  date: string;
+  icon: IconName;
+  category: string;
+}
+
+export interface GoalProps {
+  goal: Goal;
   className?: string;
 }
 
-export const Goal: React.FC<GoalsProps> = ({ className }) => {
+export const Goal: React.FC<GoalProps> = ({
+  goal: { category, date, icon, price },
+  className,
+}) => {
   return (
     <div className={styles.root}>
       <ShadowCard showCard={true}>
         <div className={styles.price}>
-          550$<div className={styles.date}>12/20/20</div>
+          {price}$<div className={styles.date}>{date}</div>
         </div>
         <div className={styles.footer}>
-          <Icon icon={"calendar"}></Icon>
-          <div className={styles.category}>Holidays</div>
+          <Icon icon={icon}></Icon>
+          <div className={styles.category}>{category}</div>
         </div>
       </ShadowCard>
     </div>
