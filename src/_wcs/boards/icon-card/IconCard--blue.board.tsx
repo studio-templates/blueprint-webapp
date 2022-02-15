a98e2341fa52979902d823da9cd3f7639a6c543f
mikawix <mikan@wix.com>
2022-02-15 13:18:42 +0200

    icon cards

--- src/_wcs/boards/icon-card/IconCard--blue.board.tsx
+++ src/_wcs/boards/icon-card/IconCard--blue.board.tsx
@@ -4,6 +4,6 @@ import { createBoard } from '@wixc3/react-board';
 import { IconCard } from '../../../components/icon-card/icon-card';
 
 export default createBoard({
-    name: 'IconCard',
-    Board: () => <IconCard icon="shopping-cart" color={'#F8964C'} backgroundColor={'#FFEADA'} />
+    name: 'IconCard -blue',
+    Board: () => <IconCard icon="airplane" color={'#70A6E8'} backgroundColor={'#E4F0FF'} />
 });
--- src/components/icon-card/icon-card.tsx
+++ src/components/icon-card/icon-card.tsx
@@ -6,22 +6,22 @@ import { MaybeElement } from "@blueprintjs/core";
 import { IconName } from "@blueprintjs/icons";
 
 export interface IconCardProps {
-  className?: string;
-  icon: IconName | MaybeElement;
-  color: string;
-  backgroundColor: string;
+    className?: string;
+    icon: IconName | MaybeElement;
+    color: string;
+    backgroundColor: string;
 }
 
 export const IconCard: React.FC<IconCardProps> = ({
-  icon,
-  color,
-  backgroundColor,
+    icon,
+    color,
+    backgroundColor,
 }) => {
-  return (
-    <div className={styles.root}>
-      <Card className={styles.IconCard} style={{ color, backgroundColor }}>
-        <Icon icon={icon} />
-      </Card>
-    </div>
-  );
+    return (
+        <div className={styles.root}>
+            <Card className={styles.IconCard} style={{ color, backgroundColor }}>
+                <Icon icon={icon} className={styles.IconSize} />
+            </Card>
+        </div>
+    );
 };
