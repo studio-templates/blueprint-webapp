c355ef6170402c1fbd63a56e464b84b838ab49de
Ari Kishon <ariki@wix.com>
2022-02-16 18:44:35 +0200

    fix: self closing sign-in board

--- _wcs/boards/SignIn/SignIn.board.tsx
+++ _wcs/boards/SignIn/SignIn.board.tsx
@@ -4,7 +4,7 @@ import { SignIn } from '../../../src/components/sign-in/sign-in';
 
 export default createBoard({
     name: 'Example of a sign-in panel',
-    Board: () => <SignIn></SignIn>,
+    Board: () => <SignIn/>,
     environmentProps: {
         windowHeight: 600,
         windowWidth: 800,
