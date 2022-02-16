3d55ba540c2c88d64f4b29524bb15bf636e42ffa
Ari Kishon <ariki@wix.com>
2022-02-16 18:43:08 +0200

    fix: remove starting newLines from boards

--- _wcs/boards/GiftCard/GiftCard.board.tsx
+++ _wcs/boards/GiftCard/GiftCard.board.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { createBoard } from '@wixc3/react-board';
 import { GiftCard } from '../../../src/components/gift-card/gift-card';
--- _wcs/boards/ProductItem/ProductItem.board.tsx
+++ _wcs/boards/ProductItem/ProductItem.board.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { createBoard } from '@wixc3/react-board';
 import { ProductItem } from '../../../src/components/product-item/product-item';
