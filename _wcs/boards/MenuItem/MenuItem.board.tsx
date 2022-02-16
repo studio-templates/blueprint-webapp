4579028ad1cbf9e23bf0b2c65cf8b4cebc9eeb32
Ari Kishon <37385467+Ari-Kishon@users.noreply.github.com>
2022-02-16 15:57:28 +0200

    chore: remove all boards for blueprint re-exports (#17)

--- _wcs/boards/MenuItem/MenuItem.board.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import { createBoard } from '@wixc3/react-board';
-import { MenuItem } from '../../../src/components/menu-item/menu-item';
-
-export default createBoard({
-    name: 'MenuItem',
-    Board: () => <MenuItem icon="emoji" text={'I am the Text!'} />
-});
--- _wcs/boards/Spinner/Spinner.board.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import { createBoard } from '@wixc3/react-board';
-import { Spinner } from '../../../src/components/spinner/spinner';
-
-export default createBoard({
-    name: 'Spinner',
-    Board: () => <Spinner />
-});
