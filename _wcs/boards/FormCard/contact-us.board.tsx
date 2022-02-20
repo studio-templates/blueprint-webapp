530403aacd914030d8d102f1187b8f1cb381be05
Ari Kishon <ariki@wix.com>
2022-02-20 20:32:27 +0200

    feat: add more boards for form card

--- _wcs/boards/FormCard/contact-us.board.tsx
+++ _wcs/boards/FormCard/contact-us.board.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createBoard } from '@wixc3/react-board';
 import { FormCard } from '../../../src/components/form-card/form-card';
+import logo from '../../../src/components/form-card/assets/logo.png';
 
 export default createBoard({
-    name: 'Example of a sign-in card',
-    Board: () => <FormCard/>,
+    name: 'Contact-us card',
+    Board: () => <FormCard formType='contact' logoURL={logo} />,
     environmentProps: {
         windowHeight: 600,
         windowWidth: 800,
