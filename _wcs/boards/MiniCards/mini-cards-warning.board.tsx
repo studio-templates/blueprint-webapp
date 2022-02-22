dcfbc17cd22d632d8be3c5fbfcb0c3fecd26d053
Ari Kishon <ariki@wix.com>
2022-02-22 16:05:49 +0200

    chore: new boards for mini-cards

--- _wcs/boards/MiniCards/mini-cards-warning.board.tsx
+++ _wcs/boards/MiniCards/mini-cards-warning.board.tsx
@@ -3,8 +3,8 @@ import { createBoard } from '@wixc3/react-board';
 import { MiniCards } from '../../../src/components/mini-cards/mini-cards';
 
 export default createBoard({
-    name: 'Example of mini cards',
-    Board: () => <MiniCards />,
+    name: 'Mini Cards - Warning',
+    Board: () => <MiniCards intent='warning' />,
     environmentProps: {
         canvasWidth: 600,
         windowHeight: 800,
