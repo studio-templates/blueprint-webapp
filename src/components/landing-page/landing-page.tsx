6003597671da04ec2cbbaec7dc51073cd1428576
Ari Kishon <ariki@wix.com>
2022-02-22 16:14:36 +0200

    fix: name of board

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -6,11 +6,10 @@ import styles from './landing-page.module.scss';
 export interface LandingPageProps {
     videoURL?: string;
     posterURL?: string
-
 }
 
 /**
- * This Product Item component is a simple demo component to showcase the capabilities of Wix Component Studio, 
+ * This Landing Page component is a simple demo component to showcase the capabilities of Wix Component Studio, 
  * it is a basic implementation, which is not meant to be utilized in a production environment.
  * 
  * Use this demo to get a feel for how easy and fun it is to create and edit components in WCS using Blueprint.js, a 3rd party React-based UI toolkit.
