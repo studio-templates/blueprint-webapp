4fe3e3795fd95d9ab64282796765c88a33afe0ec
Omer <omerk@wix.com>
2022-02-28 18:27:22 +0200

    add state for Switch

--- src/components/switch-composite/switch-composite.tsx
+++ src/components/switch-composite/switch-composite.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './switch-composite.module.scss'
 import { Switch } from '../switch/switch';
 
 export interface SwitchCompositeProps {
     className?: string;
+    activeCard: boolean;
 }
 
-export const SwitchComposite: React.FC<SwitchCompositeProps> = ({ className }) => {
+export const SwitchComposite: React.FC<SwitchCompositeProps> = ({ activeCard, className }) => {
+    const [active, setActive] = useState(false);
+
     return <div className={styles.root}>
-        <Switch className={styles.switch} />Deactivate card</div>;
+        <Switch className={styles.switch} checked={active} onChange={() => setActive(!active)}/>
+        {active ? 'Deactivate card' : 'Active card'}
+    </div>;
 };
