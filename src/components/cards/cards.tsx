9218e81f22130aa0d8679165acb2bd7381abe299
anat <anatf@wix.com>
2022-03-09 11:11:36 +0200

    reecreate changes

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -29,9 +29,7 @@ export const Cards: React.FC<CardsProps> = ({
                     <div className={styles["left"]}>
                         <CreditCard
                             cardInfo={cardInfo}
-                        />
-                        <WeeklyPayment1 />
-                    </div>
+                        /></div>
                     <Divider color="E8E8E8" className={styles.divider} />
                     <div className={styles["right"]}>
                         <Statistics price={balancePrice} type="balance" />
