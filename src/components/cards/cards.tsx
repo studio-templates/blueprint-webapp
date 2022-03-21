95671972ab3469dbfab14ad7e206fa3d1f9f8399
anat <anatf@wix.com>
2022-03-21 17:24:10 +0200

    added featture to cards

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -7,6 +7,7 @@ import { Divider } from "../divider/divider";
 import { CreditCard, CardInfo } from "../credit-card/credit-card";
 import { WeeklyPayment1 } from '../weekly-payment-1/weekly-payment-1';
 import { Weeklypayment } from '../weeklypayment/weeklypayment';
+import { WeeklyPayments } from '../weekly-payments/weekly-payments';
 
 export interface CardsProps {
     className?: string;
@@ -30,7 +31,9 @@ export const Cards: React.FC<CardsProps> = ({
                     <div className={styles["left"]}>
                         <CreditCard
                             cardInfo={cardInfo}
-                        /></div>
+                        />
+                        <WeeklyPayments />
+                    </div>
                     <Divider color="E8E8E8" className={styles.divider} />
                     <div className={styles["right"]}>
                         <Statistics price={balancePrice} type="balance" />
