e78619fe2483f6cf57e086c89a4ed15b9de4b2da
anat <anatf@wix.com>
2022-03-29 15:58:17 +0300

    added weekly payments

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -5,6 +5,7 @@ import { SwitchComposite } from "../switch-composite/switch-composite";
 import { Card } from "../card/card";
 import { Divider } from "../divider/divider";
 import { CreditCard, CardInfo } from "../credit-card/credit-card";
+import { Weeklypayment } from '../weeklypayment/weeklypayment';
 
 export interface CardsProps {
     className?: string;
@@ -28,7 +29,9 @@ export const Cards: React.FC<CardsProps> = ({
                     <div className={styles["left"]}>
                         <CreditCard
                             cardInfo={cardInfo}
-                        /></div>
+                        />
+                        <Weeklypayment />
+                    </div>
                     <Divider color="E8E8E8" className={styles.divider}></Divider>
                     <div className={styles["right"]}>
                         <Statistics price={balancePrice} type="balance" />
---
+++ src/components/weeklypayment/weeklypayment.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './weeklypayment.module.scss'
+import { ProgressBar } from '../progress-bar/progress-bar';
+
+export interface WeeklypaymentProps {
+    className?: string;
+}
+
+export const Weeklypayment: React.FC<WeeklypaymentProps> = ({ className }) => {
+    return <div className={styles.root}>
+        <ProgressBar animate={false} stripes={false} value={0.3} intent="primary" />
+        <div className={styles.footer}>
+            <div className={styles.title}>
+                Weeklypayment</div>
+            <div>
+                Text</div>
+        </div></div>;
+};
