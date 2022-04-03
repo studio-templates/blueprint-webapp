e1dd6882a360b41b569dda8612fa7a2f07329be0
Cario Wix <cario@wix.com>
2022-04-03 11:39:27 +0300

    chore: modified gift-card boards (#31)
    Co-authored-by: Cario <carioh@wix.com>

--- _wcs/boards/GiftCard/gift-card.board.tsx
+++ _wcs/boards/GiftCard/gift-card.board.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { createBoard } from '@wixc3/react-board';
 import { GiftCard } from '../../../src/components/gift-card/gift-card';
-import photoURL from '../../../src/components/gift-card/assets/img-2.jpg';
 import styles from './gift-card.board.module.scss';
 
 export default createBoard({
     name: 'Example of a gift card panel',
     Board: () => <div className={styles.grid2col}>
-                    <GiftCard photoURL={photoURL}/>
                     <GiftCard/>
+                    <GiftCard full/>
                 </div>,
     environmentProps: {
         windowHeight: 700,
