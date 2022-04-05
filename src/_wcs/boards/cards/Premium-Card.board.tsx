c2349037f3a3f0d95b3a175cc9ec88d03c2e9ff0
mikawix <mikan@wix.com>
2022-04-05 11:26:26 +0300

    names update

--- src/_wcs/boards/cards/Premium-Card.board.tsx
+++ src/_wcs/boards/cards/Premium-Card.board.tsx
@@ -5,8 +5,8 @@ import { Cards } from '../../../components/cards/cards';
 export default createBoard({
     name: 'Premium Card',
     Board: () => <Cards incomePrice={1500.55} outcomePrice={350} balancePrice={2800} cardInfo={{
-        cardHolder: 'Assaf Yonai',
-        company: 'Spotify',
+        cardHolder: 'Tom Smith',
+        company: 'Cloud Cash',
         expiryDate: '06/23',
         premium: true
     }} />,
