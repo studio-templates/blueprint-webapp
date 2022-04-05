461818a5701d0a3d340860356ea6ba6fe3b7f820
anat <anatf@wix.com>
2022-04-05 10:55:28 +0300

    fixed back

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -5,7 +5,7 @@ import { SwitchComposite } from "../switch-composite/switch-composite";
 import { Card } from "../card/card";
 import { Divider } from "../divider/divider";
 import { CreditCard, CardInfo } from "../credit-card/credit-card";
-import { Weeklypay1 } from '../weeklypay-1/weeklypay-1';
+
 
 export interface CardsProps {
     className?: string;
@@ -29,9 +29,7 @@ export const Cards: React.FC<CardsProps> = ({
                     <div className={styles["left"]}>
                         <CreditCard
                             cardInfo={cardInfo}
-                        />
-                        <Weeklypay1 />
-                    </div>
+                        /></div>
                     <Divider color="E8E8E8" className={styles.divider}></Divider>
                     <div className={styles["right"]}>
                         <Statistics price={balancePrice} type="balance" />
