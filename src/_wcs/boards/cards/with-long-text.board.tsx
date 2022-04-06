16acb49fefcb741a0ec0964582f193df52454726
anat <anatf@wix.com>
2022-04-06 12:26:04 +0300

    fixed UI of cards

--- src/_wcs/boards/cards/with-long-text.board.tsx
+++ src/_wcs/boards/cards/with-long-text.board.tsx
@@ -3,8 +3,8 @@ import { createBoard } from '@wixc3/react-board';
 import { Cards } from '../../../components/cards/cards';
 
 export default createBoard({
-    name: 'Premium Card',
-    Board: () => <Cards incomePrice={1500.55} outcomePrice={350} balancePrice={2800} cardInfo={{
+    name: 'with long text',
+    Board: () => <Cards incomePrice={1500.55} outcomePrice={350} balancePrice={28000000} cardInfo={{
         cardHolder: 'Anat Fennig',
         company: 'Spotify',
         expiryDate: '06/23',
