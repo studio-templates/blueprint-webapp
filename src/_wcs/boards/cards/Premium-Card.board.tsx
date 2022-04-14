72808e69b608c1222792360607610a9f0d996231
anat <anatf@wix.com>
2022-04-14 11:45:07 +0300

    ui fixes of cards component

--- src/_wcs/boards/cards/Premium-Card.board.tsx
+++ src/_wcs/boards/cards/Premium-Card.board.tsx
@@ -5,7 +5,7 @@ import { Cards } from '../../../components/cards/cards';
 export default createBoard({
     name: 'Premium Card',
     Board: () => <Cards incomePrice={1500.55} outcomePrice={350} balancePrice={2800} cardInfo={{
-        cardHolder: 'Assaf Yonai',
+        cardHolder: 'Anat Yonai',
         company: 'Spotify',
         expiryDate: '06/23',
         premium: true
