52f2c10c4cc5ceb2294c2b35bcad5df643643a85
anat <anatf@wix.com>
2022-04-14 11:53:03 +0300

    added weeklypay to cards

---
+++ src/_wcs/boards/weekly-payments/weekly-payments.board.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createBoard } from '@wixc3/react-board';
+import { WeeklyPayments } from '../../../components/weekly-payments/weekly-payments';
+
+export default createBoard({
+    name: 'WeeklyPayments',
+    Board: () => <WeeklyPayments />,
+    environmentProps: {
+        canvasWidth: 514
+    }
+});
--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -5,6 +5,7 @@ import { SwitchComposite } from "../switch-composite/switch-composite";
 import { Card } from "../card/card";
 import { Divider } from "../divider/divider";
 import { CreditCard, CardInfo } from "../credit-card/credit-card";
+import { WeeklyPayments } from '../weekly-payments/weekly-payments';
 
 
 export interface CardsProps {
@@ -29,7 +30,9 @@ export const Cards: React.FC<CardsProps> = ({
                     <div className={styles["left"]}>
                         <CreditCard
                             cardInfo={cardInfo}
-                        /></div>
+                        />
+                        <WeeklyPayments />
+                    </div>
                     <Divider color="E8E8E8" className={styles.divider}></Divider>
                     <div className={styles["right"]}>
                         <Statistics price={balancePrice} type="balance" />
