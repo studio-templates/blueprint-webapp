79962492254f813247f24b781483d0a431e78912
Jonathan Marton <jonathan.marton@gmail.com>
2022-04-19 14:17:50 +0300

    complicating cards component

---
+++ src/components/cards/cards-content.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from "./cards.module.scss";
+
+import { Divider } from "../divider/divider";
+
+export interface CardsContentProps {
+    leftSideRender: React.ReactNode;
+    rightSideRender: React.ReactNode;
+}
+
+export const CardsContent: React.FC<CardsContentProps> = ({leftSideRender, rightSideRender}) => {
+    return (
+        <div className={`${styles.layout} ${styles.content}`}>
+            <>
+                <div className={styles["left"]}>
+                    {leftSideRender}
+                </div>
+                <Divider color="E8E8E8" className={styles.divider}></Divider>
+                <div className={styles["right"]}>
+                    {rightSideRender}
+                </div>
+            </>
+        </div>
+    )
+}
--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import styles from "./cards.module.scss";
-import { Statistics } from "../statistics/statistics";
+import { Statistics, StatisticsProps } from "../statistics/statistics";
 import { SwitchComposite } from "../switch-composite/switch-composite";
 import { Card } from "../card/card";
-import { Divider } from "../divider/divider";
 import { CreditCard, CardInfo } from "../credit-card/credit-card";
+import { CardsContent } from './cards-content'
+
+const createStatisticsData = (prices: Array<number | undefined>): Array<StatisticsProps>  => {
+    const types: Array<'balance' | 'income' | 'outcome'> = ['balance',  'income', 'outcome']
+    return types.map((t,i) => ({price: prices[i], type: t})) 
+}
+
+export const StatisticsComp: React.FC<{data: Array<StatisticsProps>}> = ({data}) => {
+    return <div className={styles["statistics"]}>{data.map((stat, i) => <Statistics key={'stat'+i} price={stat.price} type={stat.type} className={stat.className}/>)}</div>
+}
 
 
 export interface CardsProps {
@@ -21,22 +30,17 @@ export const Cards: React.FC<CardsProps> = ({
     balancePrice,
     cardInfo,
 }) => {
+    const statisticsData = createStatisticsData([balancePrice, incomePrice, outcomePrice])
     return (
         <div className={styles.root}>
             <Card className={styles.outline}>
                 <p className={styles.title}>Cards</p>
                 <div className={`${styles.layout} ${styles.content}`}>
-                    <div className={styles["left"]}>
-                        <CreditCard
-                            cardInfo={cardInfo}
-                        /></div>
-                    <Divider color="E8E8E8" className={styles.divider}></Divider>
-                    <div className={styles["right"]}>
-                        <Statistics price={balancePrice} type="balance" />
-                        <Statistics price={incomePrice} type="income" />
-                        <Statistics price={outcomePrice} type="outcome" />
-                        <SwitchComposite />
-                    </div></div>
+                    <CardsContent 
+                        leftSideRender={<CreditCard cardInfo={cardInfo}/>} 
+                        rightSideRender={<><StatisticsComp data={statisticsData}/><SwitchComposite /></>}>
+                    </CardsContent>
+                </div>
             </Card>
         </div>
     );
