b683316a51b3b39a87f7bf9687c0b55ef22d1302
Omer <omerk@wix.com>
2022-04-28 09:37:24 +0300

    ew weekly pay comp + added to cards

--- src/components/cards/cards-content.tsx
+++ src/components/cards/cards-content.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import styles from "./cards.module.scss";
 
 import { Divider } from "../divider/divider";
+import { WeeklyPayment } from '../weekly-payment/weekly-payment';
 
 export interface CardsContentProps {
     leftSideRender?: React.ReactNode;
     rightSideRender?: React.ReactNode;
 }
 
-export const CardsContent: React.FC<CardsContentProps> = ({leftSideRender, rightSideRender, children}) => {
+export const CardsContent: React.FC<CardsContentProps> = ({ leftSideRender, rightSideRender, children }) => {
     return (
         <div className={`${styles.layout} ${styles.content}`}>
             <>
                 <div className={styles["left"]}>
                     {leftSideRender}
+                    <WeeklyPayment />
                 </div>
                 <Divider color="E8E8E8" className={styles.divider}></Divider>
                 <div className={styles["right"]}>
