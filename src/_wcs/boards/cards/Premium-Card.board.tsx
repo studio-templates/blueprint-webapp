d45566b7ab2c3f76a2f676431ea5a8a67080b51e
Omer <omerk@wix.com>
2022-04-28 09:30:42 +0300

    Fixed styling of Cards comp

--- src/_wcs/boards/cards/Premium-Card.board.tsx
+++ src/_wcs/boards/cards/Premium-Card.board.tsx
@@ -4,9 +4,9 @@ import { Cards } from '../../../components/cards/cards';
 
 export default createBoard({
     name: 'Premium Card',
-    Board: () => <Cards incomePrice={1500.55} outcomePrice={350} balancePrice={2800} cardInfo={{
-        cardHolder: 'Assaf Yonai',
-        company: 'Spotify',
+    Board: () => <Cards incomePrice={1500.55} outcomePrice={250} balancePrice={20} cardInfo={{
+        cardHolder: 'Tal Gadot',
+        company: 'Wix',
         expiryDate: '06/23',
         premium: true
     }} />,
