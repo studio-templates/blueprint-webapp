035bcbd6a5128f6a2856f3af6788c0abcdad89f8
omerken <omerk@wix.com>
2022-06-27 19:44:00 +0300

    new styles for mobile

--- src/_wcs/boards/cards/mobile.board.tsx
+++ src/_wcs/boards/cards/mobile.board.tsx
@@ -6,7 +6,7 @@ export default createBoard({
     name: 'Mobile',
     Board: () => <Cards incomePrice={1500} outcomePrice={350} balancePrice={20} cardInfo={{
         cardHolder: 'Omer Kenet ',
-        company: 'Tikal',
+        company: 'Wix',
         expiryDate: '06/23',
         premium: true
     }} />,
