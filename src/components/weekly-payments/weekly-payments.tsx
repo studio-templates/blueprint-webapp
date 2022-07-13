e441c585339f11f1367297351856fea99f3b5336
omerken <omerk@wix.com>
2022-07-13 20:21:52 +0300

    created new Weekly comp

---
+++ src/components/weekly-payments/weekly-payments.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './weekly-payments.module.scss'
+import { ProgressBar } from '../progress-bar/progress-bar';
+
+export interface WeeklyPaymentsProps {
+    className?: string;
+}
+
+export const WeeklyPayments: React.FC<WeeklyPaymentsProps> = ({ className }) => {
+    return <div className={styles.root}>
+        <ProgressBar animate={false} stripes={false} intent="primary" value={0.3} />
+        <div className={styles.title}>
+            WeeklyPayments</div></div>;
+};
