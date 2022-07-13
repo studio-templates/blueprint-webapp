3ebf759a661b4dd6fad5b4993d57c1b4f30254c1
omerken <omerk@wix.com>
2022-07-13 20:16:47 +0300

    Fixed Card component

--- src/_wcs/boards/cards/Standard-Card.board.tsx
+++ src/_wcs/boards/cards/Standard-Card.board.tsx
@@ -4,15 +4,15 @@ import { Cards } from '../../../components/cards/cards';
 
 export default createBoard({
     name: 'Standard Card',
-    Board: () => <Cards incomePrice={1500} outcomePrice={350} balancePrice={2800} cardInfo={{
-        cardHolder: 'Omer Kenet',
+    Board: () => <Cards incomePrice={1500} outcomePrice={350} balancePrice={28} cardInfo={{
+        cardHolder: 'Omer',
         company: 'Wix',
         expiryDate: '06/25',
     }} />,
     environmentProps: {
         canvasHeight: 364,
         canvasWidth: 597,
-        windowWidth: 926,
+        windowWidth: 938,
         canvasBackgroundColor: '#ffffff',
     }
 });
