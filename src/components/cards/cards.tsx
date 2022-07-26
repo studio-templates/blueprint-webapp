5bab7edeb02ffd8b72e8e48d1ceeb7a44a9c29ba
omerken <omerk@wix.com>
2022-07-26 12:00:41 +0300

    Recreate Changes

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -30,9 +30,7 @@ export const Cards: React.FC<CardsProps> = ({
                     <div className={styles["left"]}>
                         <CreditCard
                             cardInfo={cardInfo}
-                        ></CreditCard>
-                        <NewComponent2 />
-                    </div>      <Divider color="E8E8E8" className={styles.divider}></Divider>
+                        ></CreditCard></div>      <Divider color="E8E8E8" className={styles.divider}></Divider>
                     <div className={styles["right"]}>
                         <Statistics price={balancePrice} type="balance" />
                         <Statistics price={incomePrice} type="income" />
